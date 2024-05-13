9e3398f9237b8c16a2f695a6697f246acd032cce
Jo Walsh <jowals@ceh.ac.uk>
2024-05-13 16:52:04 +0100

    Test for demo packages and install; fix tiny typo

--- run.R
+++ run.R
@@ -1,4 +1,21 @@
 #!/usr/bin/env Rscript
+
+pkgload <- function() {
+  packages = c("targets", "dplyr","ggplot2","readr","visNetwork")
+  packagecheck <- match( packages, utils::installed.packages()[,1] )
+  packagestoinstall <- packages[ is.na( packagecheck ) ]
+
+  if( length( packagestoinstall ) > 0L ) {
+    utils::install.packages( packagestoinstall,
+                             repos = "http://www.stats.bris.ac.uk/R/"
+    )
+  } else {
+    print( "All requested packages already installed" )
+  }
+}
+
+pkgload()
+
 library(targets)
 library(ggplot2)
 
