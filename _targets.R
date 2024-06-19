e943cf88effe822b831da5947334336f12c4d8fc
ADCEH\plevy <plevy@ceh.ac.uk>
2024-06-19 12:40:59 +0100

    Update for CEEDS webinar

--- _targets.R
+++ _targets.R
@@ -3,8 +3,8 @@ library(targets)
 tar_option_set(packages = c("readr", "dplyr", "ggplot2"))
 tar_source()
 list(
-     tar_target(file, "data.csv", format = "file"), 
-     tar_target(data, get_data(file)), 
+     tar_target(filename, "data.csv", format = "file"), 
+     tar_target(data, get_data(filename)), 
      tar_target(model,fit_model(data)), 
      tar_target(plot, plot_model(model, data))
 )
